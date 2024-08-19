da3f6e97e7ecc116982af8405611068fb97d4be2
Girishvarlyani <varlyanigirish123@gmail.com>
2024-08-19 09:26:50 +0530

    added index

--- src/DisplayQus/DisplayQus.jsx
+++ src/DisplayQus/DisplayQus.jsx
@@ -6,7 +6,7 @@ import styles from './DisplayQus.module.css'
 export const DisplayQus=()=> {
   return (
     <div>
-        {questions.map(qus=>{
+        {questions.map((qus,index)=>{
             return <div key={qus.id} className={styles.buttons}><Button  questions={qus.question} id={qus.id}/></div>
         })}
     </div>
